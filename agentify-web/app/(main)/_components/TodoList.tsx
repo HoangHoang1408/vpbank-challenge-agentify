39b90382af204b8f0bc8c1f60bbc957e07afc4a5
Liam <liam.nguyen306@gmail.com>
2025-11-06 23:46:46 +0700

    feat: add no data note to todo list

--- agentify-web/app/(main)/_components/TodoList.tsx
+++ agentify-web/app/(main)/_components/TodoList.tsx
@@ -68,72 +68,89 @@ const TodoList: FC = () => {
           </Typography.Text>
         </div>
 
-        <Space direction="vertical" size={12} className="w-full">
-          {mockData.map((todo) => (
-            <Card key={todo.id} size="small" className="w-full rounded-xl!">
-              <div className="flex justify-between">
-                <div className="flex items-center gap-2">
-                  <Button
-                    size="small"
-                    shape="circle"
-                    icon={
-                      todo.isCompleted ? (
-                        <LuCheck className="w-3.5 h-3.5" />
-                      ) : null
-                    }
-                    variant={todo.isCompleted ? 'solid' : 'outlined'}
-                    color="primary"
-                    className="w-4! min-w-4! h-4! p-0! mt-1.25! mb-auto!"
-                  />
+        {mockData.length > 0 ? (
+          <Space direction="vertical" size={12} className="w-full">
+            {mockData.map((todo) => (
+              <Card key={todo.id} size="small" className="w-full rounded-xl!">
+                <div className="flex justify-between">
+                  <div className="flex items-center gap-2">
+                    <Button
+                      size="small"
+                      shape="circle"
+                      icon={
+                        todo.isCompleted ? (
+                          <LuCheck className="w-3.5 h-3.5" />
+                        ) : null
+                      }
+                      variant={todo.isCompleted ? 'solid' : 'outlined'}
+                      color="primary"
+                      className="w-4! min-w-4! h-4! p-0! mt-1.25! mb-auto!"
+                    />
 
-                  <div>
-                    <Typography.Title
-                      level={3}
-                      className={cn(
-                        'font-semibold text-sm! sm:text-base! mb-1!',
-                        todo.isCompleted && 'line-through text-text-tertiary!',
-                      )}
-                    >
-                      {todo.title}
-                    </Typography.Title>
-                    <Typography.Text
-                      type="secondary"
-                      className={cn(
-                        'text-xs! sm:text-sm! mb-2!',
-                        todo.isCompleted && 'line-through',
-                      )}
-                    >
-                      {todo.description}
-                    </Typography.Text>
+                    <div>
+                      <Typography.Title
+                        level={3}
+                        className={cn(
+                          'font-semibold text-sm! sm:text-base! mb-1!',
+                          todo.isCompleted &&
+                            'line-through text-text-tertiary!',
+                        )}
+                      >
+                        {todo.title}
+                      </Typography.Title>
+                      <Typography.Text
+                        type="secondary"
+                        className={cn(
+                          'text-xs! sm:text-sm! mb-2!',
+                          todo.isCompleted && 'line-through',
+                        )}
+                      >
+                        {todo.description}
+                      </Typography.Text>
 
-                    <div className="flex gap-4 mt-2">
-                      <div className="flex items-center gap-1">
-                        <LuUser className="w-3 h-3 text-text-tertiary!" />
-                        <Typography.Text
-                          type="secondary"
-                          className="text-xs! mb-0.25!"
-                        >
-                          {todo.category}
-                        </Typography.Text>
-                      </div>
-                      <div className="flex items-center gap-1">
-                        <LuCalendar className="w-3 h-3 text-text-tertiary!" />
-                        <Typography.Text
-                          type="secondary"
-                          className="text-xs! mb-0.25!"
-                        >
-                          {dayjs(todo.createdAt).format('DD/MM/YYYY')}
-                        </Typography.Text>
+                      <div className="flex gap-4 mt-2">
+                        <div className="flex items-center gap-1">
+                          <LuUser className="w-3 h-3 text-text-tertiary!" />
+                          <Typography.Text
+                            type="secondary"
+                            className="text-xs! mb-0.25!"
+                          >
+                            {todo.category}
+                          </Typography.Text>
+                        </div>
+                        <div className="flex items-center gap-1">
+                          <LuCalendar className="w-3 h-3 text-text-tertiary!" />
+                          <Typography.Text
+                            type="secondary"
+                            className="text-xs! mb-0.25!"
+                          >
+                            {dayjs(todo.createdAt).format('DD/MM/YYYY')}
+                          </Typography.Text>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
 
-                <Button shape="circle" icon={<LuTrash2 />} type="text" danger />
-              </div>
-            </Card>
-          ))}
-        </Space>
+                  <Button
+                    shape="circle"
+                    icon={<LuTrash2 />}
+                    type="text"
+                    danger
+                  />
+                </div>
+              </Card>
+            ))}
+          </Space>
+        ) : (
+          <div className="mt-12 mb-8">
+            <Typography.Paragraph
+              type="secondary"
+              className="text-sm! text-center"
+            >
+              No tasks yet. Chat with the AI to create your first task!
+            </Typography.Paragraph>
+          </div>
+        )}
       </Card>
     </div>
   );
