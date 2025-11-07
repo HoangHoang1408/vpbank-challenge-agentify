23243cb6f54078d2a93fd66c9b9e4b8286828fb6
Liam <liam.nguyen306@gmail.com>
2025-11-08 00:34:51 +0700

    feat: mark task is completed

--- agentify-web/app/(main)/_components/TodoList.tsx
+++ agentify-web/app/(main)/_components/TodoList.tsx
@@ -1,23 +1,16 @@
 'use client';
 
 import { useGetTasksQuery } from '@/lib/api';
-import { cn } from '@/lib/utils';
 import { Button, Card, Space, Typography } from 'antd';
 import dayjs from 'dayjs';
 import { FC, useState } from 'react';
-import {
-  LuCalendar,
-  LuCheck,
-  LuChevronsDown,
-  LuChevronsUp,
-  LuTrash2,
-  LuUser,
-} from 'react-icons/lu';
+import { LuChevronsDown, LuChevronsUp } from 'react-icons/lu';
+import TodoCard from './TodoCard';
 
 const TodoList: FC = () => {
   const [showAll, setShowAll] = useState(false);
 
-  const { data: tasks } = useGetTasksQuery({
+  const { data: tasks, refetch: refetchTasks } = useGetTasksQuery({
     rmId: 1,
   });
 
@@ -55,80 +48,11 @@ const TodoList: FC = () => {
           <div>
             <Space direction="vertical" size={12} className="w-full">
               {filteredData.map((task) => (
-                <Card key={task.id} size="small" className="w-full rounded-xl!">
-                  <div className="flex justify-between">
-                    <div className="flex items-center gap-2">
-                      <Button
-                        size="small"
-                        shape="circle"
-                        icon={
-                          task.status === 'COMPLETED' ? (
-                            <LuCheck className="w-3.5 h-3.5" />
-                          ) : null
-                        }
-                        variant={
-                          task.status === 'COMPLETED' ? 'solid' : 'outlined'
-                        }
-                        color="primary"
-                        className="w-4! min-w-4! h-4! p-0! mt-1.25! mb-auto!"
-                      />
-
-                      <div>
-                        <Typography.Title
-                          level={3}
-                          className={cn(
-                            'font-semibold text-sm! sm:text-base! mb-1!',
-                            task.status === 'COMPLETED' &&
-                              'line-through text-text-tertiary!',
-                          )}
-                        >
-                          {task.taskDetails}
-                        </Typography.Title>
-                        <Typography.Text
-                          type="secondary"
-                          className={cn(
-                            'text-xs! sm:text-sm! mb-2!',
-                            task.status === 'COMPLETED' && 'line-through',
-                          )}
-                        >
-                          {task.taskDetails}
-                        </Typography.Text>
-
-                        <div className="flex gap-4 mt-2">
-                          <div className="flex items-center gap-1">
-                            <LuUser className="w-3 h-3 text-text-tertiary!" />
-                            <Typography.Text
-                              type="secondary"
-                              className="text-xs! mb-0.25! capitalize"
-                            >
-                              {task.taskType.toLowerCase()}
-                            </Typography.Text>
-                          </div>
-                          <div className="flex items-center gap-1">
-                            <LuCalendar className="w-3 h-3 text-text-tertiary!" />
-                            <Typography.Text
-                              type="secondary"
-                              className={cn(
-                                'text-xs! mb-0.25!',
-                                dayjs(task.dueDate).isBefore(dayjs(), 'day') &&
-                                  'text-red-500!',
-                              )}
-                            >
-                              {dayjs(task.dueDate).format('DD/MM/YYYY')}
-                            </Typography.Text>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                    <Button
-                      shape="circle"
-                      icon={<LuTrash2 />}
-                      type="text"
-                      danger
-                    />
-                  </div>
-                </Card>
+                <TodoCard
+                  key={task.id}
+                  task={task}
+                  refetchTasks={refetchTasks}
+                />
               ))}
             </Space>
 
--- agentify-web/lib/api/task.ts
+++ agentify-web/lib/api/task.ts
@@ -1,5 +1,5 @@
 import { IGetTaskParams, ITask } from '@/types';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { API } from './axios';
 
@@ -8,6 +8,14 @@ export const useGetTasksQuery = (params: IGetTaskParams) =>
     queryKey: ['tasks', params],
     queryFn: async () => {
       const response = await API.get<ITask[]>('tasks', { params });
+      return response.data;
+    },
+  });
+
+export const useMarkTaskAsCompletedMutation = () =>
+  useMutation<ITask, AxiosError, number>({
+    mutationFn: async (taskId: number) => {
+      const response = await API.delete<ITask>(`tasks/${taskId}/soft`);
       return response.data;
     },
   });
